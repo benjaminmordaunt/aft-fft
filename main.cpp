6837e2d79703c103d1f438256a99a8b4bb7653b4
Benjamin Mordaunt <crawford.benjamin15@gmail.com>
2023-09-13 03:36:10 +0100

    Initial working implementation for 1D-DFT.
    Requires GCEM header-only library.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
  */
 
 #include <iostream>
-#include <cmath>
+#include <gcem.hpp>
 #include <fftw3.h>
 
 static constexpr bool ISP2(const unsigned int x) {
@@ -22,32 +22,36 @@ namespace aft {
     template<unsigned int M, typename T>
     struct AftFFT {
         static_assert(ISP2(M), "Only power-of-2 DFT vector lengths supported.");
-        explicit AftFFT(aft_complex<T> const (&in)[M]) : m_in(in) {}
+        explicit AftFFT(aft_complex<T> const (&in)[M]) : m_in(in), m_out{0} {}
         aft_complex<T> const (&m_in)[M];
         aft_complex<T> m_out[M];
 
         template<unsigned int N>
-        __attribute__((always_inline)) inline void dft(unsigned int ii,
-                                                       unsigned int oi) {
-            T romega, iomega;
+        __attribute__((always_inline)) inline void dft(unsigned int i, unsigned int stride) {
+            T romega, iomega, er, ei, or_, oi, ur, ui;
 
             if (N == 1) {
-                m_out[oi] = m_in[ii];
+                m_out[i].real = m_in[i].real;
+                m_out[i].imag = m_in[i].imag;
                 return;
             }
 
-            dft<N/2>(ii, oi);
-            dft<N/2>(ii+1, oi+1);
+            dft<N/2>(i, stride*2);
+            dft<N/2>(i+stride, stride*2);
 
-            for (int i = 0; i < N/2; i++) {
-                romega = cos(-i * M_2_PI / N);
-                iomega = sin(-i * M_2_PI / N);
-                m_out[oi].real = m_in[ii].real + m_in[ii+1].real * romega;
-                m_out[oi+N/2].real = m_in[ii].real - m_in[ii+1].real * romega;
-                m_out[oi].imag = m_in[ii].imag + m_in[ii+1].imag * iomega;
-                m_out[oi+N/2].imag = m_in[ii].imag - m_in[ii+1].imag * iomega;
-                ii += 2;
-                oi += 2;
+            for (int j = 0; j < N/2; j++) {
+                romega = gcem::cos(-j * M_2_PI / N);
+                iomega = gcem::sin(-j * M_2_PI / N);
+                er = m_out[i + 2*j * stride].real;
+                ei = m_out[i + 2*j * stride].imag;
+                or_ = m_out[i + (2*j + 1) * stride].real;
+                oi = m_out[i + (2*j + 1) * stride].imag;
+                ur = or_ * romega - oi * iomega;
+                ui = or_ * iomega + oi * romega;
+                m_out[i + 2*j * stride].real = er + ur;
+                m_out[i + 2*j * stride].imag = ei + ui;
+                m_out[i + (2*j + 1) * stride].real = er - ur;
+                m_out[i + (2*j + 1) * stride].imag = ei - ui;
             }
         }
 
